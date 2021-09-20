323a966a7c130b136ed3e5e3a69f42343bd3df35
aKrRaC <akhilrac2000@gmail.com>
2021-09-20 20:23:48 +0530

    added addition

--- helloworld/views.py
+++ helloworld/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World!")
+    return render(request,'home.html')
+
+def add(request):
+
+    #performing addition
+    num1 = request.GET['n1']
+    num2 = request.GET['n2']
+    result = int(num1) + int(num2)
+
+
+    return render(request,'result.html',{'result':result, 'num1':num1, 'num2':num2})
