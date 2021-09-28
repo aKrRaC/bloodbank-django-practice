3854f12bc21286fadeefa8f8233bd7566be1892e
aKrRaC <akhilrac2000@gmail.com>
2021-09-28 17:05:29 +0530

    added signup, login

--- bloodbank/urls.py
+++ bloodbank/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home',views.index, name='index'),
-    path('register',views.register, name='register'),
-    path('display',views.display, name='display')
+    path('',views.index, name = 'index'),
+    path('signup',views.signup_page, name = 'signup_page'),
+    path('login',views.login_page, name = 'login_page'),
+    path('home',views.home, name = 'home_page'),
+    path('register',views.register_page, name= 'register_page'),
+    path('display',views.display, name = 'display_page'),
+    path('signup_user',views.signup, name = "signup_user"),
+    path('login_user',views.login, name = "login_user"),
+    path('register_user',views.register, name = "register_user")
 ]
--- bloodbank/views.py
+++ bloodbank/views.py
@@ -2,10 +2,60 @@ from django.shortcuts import render
 
 # Create your views here.
 reg_user_list = []
+user_list = []
 
 def index(request):
+    return render(request,'index.html')
 
-    return render(request,'index1.html')
+def signup_page(request):
+    return render(request,'signup.html')
+
+def signup(request):
+    add_user = {}
+    password1 = request.POST['pass']
+    password2 = request.POST['conpass']
+    if (password1 == password2):
+        add_user['fname'] = request.POST['fname']
+        add_user['lname'] = request.POST['lname']
+        add_user['dob'] = request.POST['dob']
+        add_user['contact'] = request.POST['contact']
+        add_user['email'] = request.POST['email']
+        add_user['pass'] = password1
+
+        user_list.append(add_user)
+        print (add_user)
+        print (user_list)
+        return render(request,'login.html')
+    else:
+        print ("pass error!")
+        return render(request,'signup.html')
+
+def login_page(request):
+    return render(request,'login.html')
+
+def login(request):
+    login_user = []
+    login_user.append(request.POST['email'])
+    login_user.append(request.POST['pass'])
+    for i in user_list:
+        if (i["email"] == login_user[0] and i["pass"] == login_user[1]):
+            return render(request,'home.html')
+        else:
+            return render(request,'login.html')
+
+def home(request):
+    login_status = request.session.get('login_status',1)
+    if (login_status == 1):
+        return render(request,'home.html')
+    else:
+        return render(request,'index.html')
+
+def register_page(request):
+    login_status = request.session.get('login_status',1)
+    if (login_status == 1):
+        return render(request,'register.html')
+    else:
+        return render(request,'index.html')
 
 def register(request):
     reg_user = {}
@@ -17,7 +67,8 @@ def register(request):
 
     reg_user_list.append(reg_user)
     print(reg_user_list)
-    return render(request,'index1.html')
+    return render(request,'display.html',{'userdet': reg_user_list})
+
 
 def display(request):
     return render(request,'display.html',{'userdet': reg_user_list})
